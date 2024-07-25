527c66a66ac2b44231a117135a414a23a3302439
Anthony Choque <140866954+Anthonymss@users.noreply.github.com>
2024-07-25 06:50:54 +0200

    Actualizar telegram

---
+++ src/main/java/com/telegram/telegram/KeepAliveController.java
@@ -0,0 +1,13 @@
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KeepAliveController {
+
+    @GetMapping("/keepalive")
+    public ResponseEntity<String> keepAlive() {
+        return ResponseEntity.ok("I'm alive!");
+    }
+}
